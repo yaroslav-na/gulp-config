bc2cef1e6b1781f7948bcf13b40bac08de3794a0
unknown <yinazarenkoo@gmail.com>
2024-10-01 13:23:53 +0200

    bug fixed

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -15,9 +15,8 @@ export function scss() {
           })
         )
       )
-      .pipe(plugins.replace('/src/assets/', '../assets/'))
       .pipe(plugins.sass({ outputStyle: 'expanded' }))
-      .pipe(plugins.groupCssMediaQueries())
+      .pipe(plugins.replace('/src/assets/', '../assets/'))
       .pipe(plugins.webpcss({ webpClass: '.webp', noWebpClass: '.no-webp' }))
       .pipe(
         plugins.autoprefixer({
